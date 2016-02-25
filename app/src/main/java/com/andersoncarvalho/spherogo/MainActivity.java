867db9af788697c19c243760df1df1d750ade08a
Anderson Carvalho <anderson@andersoncarvalho.com>
2016-02-25 15:12:16 -0300

    Sensor do aparelho implementado, conexao com a bolinha efetuada, falta receber a sphero para testar

--- app/src/main/java/com/andersoncarvalho/spherogo/MainActivity.java
+++ app/src/main/java/com/andersoncarvalho/spherogo/MainActivity.java
@@ -1,8 +1,15 @@
 package com.andersoncarvalho.spherogo;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,15 +19,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.orbotix.ConvenienceRobot;
+import com.orbotix.DualStackDiscoveryAgent;
+import com.orbotix.common.DiscoveryException;
+import com.orbotix.common.Robot;
+import com.orbotix.le.DiscoveryAgentLE;
+import com.orbotix.le.RobotLE;
+import com.orbotix.common.RobotChangedStateListener;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements RobotChangedStateListener,NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
+
+    public ConvenienceRobot mRobot;
+    public DualStackDiscoveryAgent coneccao;
+    public TextView status, posicaoX, posicaoY, posicaoZ, detalhesText;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coneccao.getInstance().addRobotStateListener(this);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        status = (TextView) findViewById(R.id.status);
+        posicaoX = (TextView) findViewById(R.id.posicaoX);
+        posicaoY = (TextView) findViewById(R.id.posicaoY);
+        posicaoZ = (TextView) findViewById(R.id.posicaoZ);
+        detalhesText = (TextView) findViewById(R.id.detalhesText);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Sphero Go");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -41,6 +77,33 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //If the DiscoveryAgent is not already looking for robots, start discovery.
+        if( !coneccao.getInstance().isDiscovering() ) {
+            try {
+                coneccao.getInstance().startDiscovery(getApplicationContext());
+            } catch (DiscoveryException e) {
+                Log.e("Sphero", "DiscoveryException: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -97,5 +160,82 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type) {
+        mRobot = new ConvenienceRobot( robot );
+        switch (type) {
+            case Online: {
+                status.setText("Status : Online");
+                float red = Color.RED/255f;
+                float green = Color.GREEN/255f;
+                float blue = Color.BLUE/255f;
+
+
+                mRobot.setLed(red,green,blue);
+                break;
+            }
+            case Connected: {
+                status.setText("Status : Conectado");
+                Log.d("Go!", "Sphero conectado");
+                Toast.makeText(getApplicationContext(), "Sphero conectado!", Toast.LENGTH_SHORT).show();
+            }
+            case Disconnected: {
+                status.setText("Status : Desconectado");
+                Log.d("Go!", "Sphero desconectado");
+                Toast.makeText(getApplicationContext(), "Sphero desconectado!", Toast.LENGTH_SHORT).show();
+            }
+            case FailedConnect: {
+                Log.d("Go!", "Falha ao conectar no Sphero");
+                Toast.makeText(getApplicationContext(), "Falha ao conectar no Sphero!", Toast.LENGTH_SHORT).show();
+            }
+            case Connecting: {
+                status.setText("Status : Conectando ...");
+                Log.d("Go!", "Conectando ao Sphero ...");
+                Toast.makeText(getApplicationContext(), "Conectando ao Sphero ...", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Float x = event.values[0];
+        Float y = event.values[1];
+        Float z = event.values[2];
+
+         /*
+        Os valores ocilam de -10 a 10.
+        Quanto maior o valor de X mais ele ta caindo para a esquerda - Positivo Esqueda
+        Quanto menor o valor de X mais ele ta caindo para a direita  - Negativo Direita
+        Se o valor de  X for 0 então o celular ta em pé - Nem Direita Nem Esquerda
+        Se o valor de Y for 0 então o cel ta "deitado"
+         Se o valor de Y for negativo então ta de cabeça pra baixo, então quanto menor y mais ele ta inclinando pra ir pra baixo
+        Se o valor de Z for 0 então o dispositivo esta reto na horizontal.
+        Quanto maioro o valor de Z Mais ele esta inclinado para frente
+        Quanto menor o valor de Z Mais ele esta inclinado para traz.
+        */
+
+        posicaoX.setText("Posição X: " + x.intValue() + " Float: " + x);
+        posicaoY.setText("Posição Y: " + y.intValue() + " Float: " + y);
+        posicaoZ.setText("Posição Z: " + z.intValue() + " Float: " + z);
+
+        if(y < 0) { // O dispositivo esta de cabeça pra baixo
+            if(x > 0)
+                detalhesText.setText("Virando para ESQUERDA ficando INVERTIDO");
+            if(x < 0)
+                detalhesText.setText("Virando para DIREITA ficando INVERTIDO");
+        } else {
+            if(x > 0)
+                detalhesText.setText("Virando para ESQUERDA ");
+            if(x < 0)
+                detalhesText.setText("Virando para DIREITA ");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
